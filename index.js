ca9927d3c22e26908f6326f1bfb9688297df8b9e
MNZT <toby@nerdping.com>
2016-02-16 18:54:03 +0000

    Added default bear.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+"use strict"
 const fs = require('fs')
 
 exports = module.exports = function (bearType) {
-  const bearFile = `${__dirname}/bears/${bearType}.jpg`
-  return fs.readFile(bearFile, (err, data) => {
+  const bear = bearType || "polar"
+  return fs.readFile(`${__dirname}/bears/${bear}.jpg`, (err, data) => {
     if (err) throw err
     console.log(data)
     return data
--- test.js
+++ test.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const tape = require('tape')
 const test = require('tape-drain')(tape)
 const aBear = require('./')
-const bear = fs.readFile('bear.jpg', (err, data) => {
+const bear = fs.readFile('bears/polar.jpg', (err, data) => {
   if (err) throw err
   return data
 })
 
 test('bear is a bear', function (t) {
-  t.drain({buffer: true}).equal(aBear, bear)
+  t.drain({buffer: true}).equal(aBear(), bear)
   t.end()
 })
